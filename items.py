812a5a631b43c0742b9a591fb9d8c1b764c1fc19
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2024-04-15 16:41:49 -0700

    Items object file added

---
+++ items.py
@@ -0,0 +1,15 @@
+items = {
+    "op_slice.png": {
+        "name": "slice",
+        "type": "throw",
+        "description": "Slice of cream pie",
+        "damage": 10
+    },
+
+    "slice.png": {
+        "name": "slice",
+        "type": "throw",
+        "description": "Slice of cream pie",
+        "damage": 10
+    }
+}
