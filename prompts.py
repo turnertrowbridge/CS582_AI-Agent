d5d2779de7387440e16abef0ab1127218f8a5e29
turnertrowbridge <ttrowbridge7322@sdsu.edu>
2024-04-24 19:05:45 -0700

    Reformatting

--- prompts.py
+++ prompts.py
@@ -6,4 +6,5 @@ past_items_prompt = "You have used the following items in the past: \n"
 # f"You have {len(items)} items"
 item_selection_prompt = ("items and you need to pick exactly 1 of them. Say back to me only the exact name of the "
                          "item in the format given with no added words or punctuation. Never use a drop type gag "
-                         "after using a lure type previously. Do not use the same item twice in a row. Here are the items, followed by their type:\n")
+                         "after using a lure type previously. Do not use the same item twice in a row. Here are"
+                         "the items, followed by their type:\n")
